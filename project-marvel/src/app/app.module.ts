2e989a160f5cdd43052d652082ac8f990bf3918c
SpaceNightt <sduncan@hotmail.fr>
2023-12-08 15:47:59 +0100

    ça fonctionne enfin

--- project-marvel/src/app/app.module.ts
+++ project-marvel/src/app/app.module.ts
@@ -3,15 +3,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MarvelComponent } from './marvel/marvel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarvelComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule // Ajoutez cette ligne pour activer le module HttpClientModule
--- project-marvel/src/app/marvel.service.ts
+++ project-marvel/src/app/marvel.service.ts
@@ -2,7 +2,9 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
 export interface MarvelCharacter {
   id: number;
   name: string;
@@ -17,13 +19,22 @@ export interface MarvelCharacter {
   providedIn: 'root'
 })
 export class MarvelService {
-  private apiUrl = 'https://gateway.marvel.com/v1/public';
-  private publicKey = 'b60afe8c1a5cb0d468c8c2b236541e64'; // Remplacez par votre clé publique
+  private apiUrl = 'https://gateway.marvel.com/v1/public/';
+  private apiKey = 'b60afe8c1a5cb0d468c8c2b236541e64'; // Remplacez par votre clé publique
+  private charactersEndpoint = 'characters';
 
   constructor(private http: HttpClient) { }
 
-  getMarvelCharacters(): Observable<any> {
-    const url = `${this.apiUrl}/characters?apikey=${this.publicKey}`;
-    return this.http.get(url);
+  getMarvelCharacters(): Observable<MarvelCharacter[]> {
+    const url = `${this.apiUrl}${this.charactersEndpoint}?apikey=${this.apiKey}`;
+    return this.http.get<any>(url).pipe(
+      map((response: any) => response.data.results),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: any): Observable<never> {
+    console.error('An error occurred', error);
+    return throwError('Something went wrong. Please try again later.');
   }
 }
