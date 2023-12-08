2a9cdda542bd89dcea5410a6e23a0917c63af33c
Jean-Baptiste Jail <jean-baptite.jail@isen.yncrea.fr>
2023-12-08 11:18:31 +0100

    modif : marvel.service.ts / home.component.ts et html

---
+++ project-marvel/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+// home.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { MarvelService, MarvelCharacter } from '../marvel.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  marvelCharacters: MarvelCharacter[] = [];
+
+  constructor(private marvelService: MarvelService) { }
+
+  ngOnInit(): void {
+    this.getMarvelCharacters();
+  }
+
+  getMarvelCharacters(): void {
+    this.marvelService.getMarvelCharacters().subscribe(characters => {
+      this.marvelCharacters = characters;
+    });
+  }
+}
--- project-marvel/src/app/marvel.service.ts
+++ project-marvel/src/app/marvel.service.ts
@@ -1,8 +1,29 @@
 // marvel.service.ts
 
+export interface MarvelApiResponse {
+  data: {
+    results: MarvelCharacter[];
+  };
+}
+
+export interface MarvelCharacter {
+  id: number;
+  name: string;
+  description: string;
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+  // Ajoute d'autres propriétés si nécessaire
+}
+
+
+// marvel.service.ts
+
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,17 +31,20 @@ import { Observable } from 'rxjs';
 export class MarvelService {
 
   private publicKey = '6c6fe75e100637151a57e763c9f9a635';
-  private apiUrl = 'https://gateway.marvel.com:443/v1/public/characters?apikey=6c6fe75e100637151a57e763c9f9a635';  // Remplace avec l'URL de l'API Marvel
+  private apiUrl = 'https://gateway.marvel.com:443/v1/public/characters';
 
   constructor(private http: HttpClient) { }
 
-  getMarvelCharacters(): Observable<any> {
+  getMarvelCharacters(): Observable<MarvelCharacter[]> {
     const ts = new Date().getTime().toString();
-    const hash = 'ffd275c5130566a2916217b101f26150';  // Remplace avec le vrai calcul du hash
+    const hash = 'ffd275c5130566a2916217b101f26150';
 
     const fullUrl = `${this.apiUrl}?ts=${ts}&apikey=${this.publicKey}&hash=${hash}`;
-    
-    return this.http.get(fullUrl);
+
+    return this.http.get<MarvelApiResponse>(fullUrl)
+      .pipe(
+        map(response => response.data.results)
+      );
   }
 
   // Ajoute d'autres méthodes selon les besoins
